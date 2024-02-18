26d86b53bbe1ec58b3d57c01783f9ebfac85efa2
rsmciftci <rsmciftci@gmail.com>
2024-02-18 15:56:52 +0000

    Rest Framework & Corsheaders installed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app'
+    'app',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [

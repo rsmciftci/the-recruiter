55297a9cda65374728266329b21d30f09e31be3b
rsmciftci <rsmciftci@gmail.com>
2024-02-28 03:21:20 +0000

    find candidate endpoint added

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -3,7 +3,7 @@ from .views import company_view, recruiter_view, jobadvert_view, candidate_views
 urlpatterns = [
     path('hello-world/', candidate_views.hello_world  , name='hello_world'),
     path('', candidate_views.ApiOverview , name='home'),
-    path('candidate/', candidate_views.add_candidate, name='add-candidate'),
+    path('candidate/', candidate_views.add_or_find_candidate, name='add_or_find_candidate'),
     path('candidate/<int:id>', candidate_views.update_or_delete_candidate, name='update_or_delete_candidate'),
     
     path("company/", company_view.add_company ),
--- backend/app/views/candidate_views.py
+++ backend/app/views/candidate_views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from ..models import Candidate
 from ..serializers.candidate_serializers import CandidateSerializer
 from rest_framework import serializers, status
+import hashlib
 
 
 @api_view(["GET"])
@@ -23,32 +24,43 @@ def ApiOverview(request):
     return Response(api_urls)
 
 
-@api_view(['POST'])
-def add_candidate(request):
-    candidate = CandidateSerializer(data=request.data)
-    candidate.password_str_to_md5(data=request.data)
+@api_view(['POST','GET'])
+def add_or_find_candidate(request):
     
-    username = request.data.get('username')
-    email = request.data.get('email')
-    phone = request.data.get('phone')
-
-    
-    
-    if Candidate.objects.filter(username=username).exists():
-        raise serializers.ValidationError('Username already exists')
-    
-    if Candidate.objects.filter(email=email).exists():
-        raise serializers.ValidationError('Email already exists')
-    
-    if Candidate.objects.filter(phone=phone).exists():
-        raise serializers.ValidationError('Phone already exists')
-
-    if candidate.is_valid():
+    if request.method == "POST":
+        candidate = CandidateSerializer(data=request.data)
+        candidate.password_str_to_md5(data=request.data)
         
-        candidate.save()
-        return Response(status=status.HTTP_201_CREATED)
-    else:
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+        username = request.data.get('username')
+        email = request.data.get('email')
+        phone = request.data.get('phone')
+
+        
+        
+        if Candidate.objects.filter(username=username).exists():
+            raise serializers.ValidationError('Username already exists')
+        
+        if Candidate.objects.filter(email=email).exists():
+            raise serializers.ValidationError('Email already exists')
+        
+        if Candidate.objects.filter(phone=phone).exists():
+            raise serializers.ValidationError('Phone already exists')
+
+        if candidate.is_valid():
+            
+            candidate.save()
+            return Response(status=status.HTTP_201_CREATED)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == "GET":
+        email = request.data.get("email")
+        password = hashlib.md5(request.data.get("password").encode("utf-8")).hexdigest() 
+        print(email)
+        try:
+            company = Candidate.objects.get(email=email, password=password)
+        except Candidate.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        return Response(status=status.HTTP_200_OK)
     
 @api_view(['PUT','DELETE'])
 def update_or_delete_candidate(request, id):    

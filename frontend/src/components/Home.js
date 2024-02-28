1a63e78eb974d27426b6138c31f677ba70cbc082
rsmciftci <rsmciftci@gmail.com>
2024-02-28 22:31:59 +0000

    import removed

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,6 +1,5 @@
 import {  useSelector } from 'react-redux';
 import HomeLogout from './HomeLogout';
-import Test from'./Test';
 
 function Home() {
     const candidateData = useSelector(state => state.candidateData)

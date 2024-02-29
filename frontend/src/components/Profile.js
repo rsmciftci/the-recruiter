455d2ef97c420e85bc87e784810c6fb3fa91dcd8
rsmciftci <rsmciftci@gmail.com>
2024-02-29 05:21:35 +0000

    template for profile component created

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,43 +1,52 @@
 import Table from 'react-bootstrap/Table';
 import { useSelector } from 'react-redux';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import Row from 'react-bootstrap/Row';
+import styles from './Profile.module.css'
+import { FaPencilAlt } from "react-icons/fa";
+
 function Profile() {
 
 
     const candidateData = useSelector(state => state.candidateData)
     return (
 
-        <div>
-            <Table striped bordered hover size="sm">
-                <thead>
-                    <tr>
-                        {console.log("PROFILE")}
-                        {console.log(candidateData.login)}
-                        <th>#</th>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Username</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td colSpan={2}>Larry the Bird</td>
-                        <td>@twitter</td>
-                    </tr>
-                </tbody>
-            </Table>
+        <div className={styles.border}>
+            <Container>
+            <FaPencilAlt className={styles.editButton} onClick={() => console.log("edit")}/>
+                <Row>
+                   
+                    <Col>
+                        <Image src="profile.png" roundedCircle className={styles.profilePhoto}/>
+                    </Col>
+                    <Table>
+                        <tr>
+                            <td className={styles.nameSurname}>Rasim CHIFTCHI</td>
+                        </tr>
+                        <tr>
+                            <td>Java Developer</td>
+                        </tr>
+                        <tr>
+                            <td>CV</td>
+                        </tr>
+                        <tr>
+                            <td>rsmchiftchi@gmail.com</td>
+                        </tr>
+                        <tr>
+                            <td>07565391538</td>
+                        </tr>
+                        <tr>
+                            <td>E15 1UB</td>
+                        </tr>
+                        <tr>
+                            <td>Stratford, London</td>
+                        </tr>
+                    </Table>
+                   
+                </Row>
+            </Container>
 
         </div>
     );

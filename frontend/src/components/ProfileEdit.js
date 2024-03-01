4b822680bbfc5fffad6b24759ed3a7215e81d3e6
rsmciftci <rsmciftci@gmail.com>
2024-03-01 08:00:27 +0000

    updated

--- frontend/src/components/ProfileEdit.js
+++ frontend/src/components/ProfileEdit.js
@@ -17,9 +17,9 @@ import { styled } from '@mui/material/styles';
 import { useState } from 'react';
 import candidateService from '../services/CandidateService';
 import { setCandidate } from '../redux/candidateSlice';
-import { Alert } from 'react-bootstrap';
 import { useEffect } from 'react';
 import AppliedJobs from './AppliedJobs';
+import Alert from 'react-bootstrap/Alert';
 
 
 const VisuallyHiddenInput = styled('input')({
@@ -37,199 +37,194 @@ const VisuallyHiddenInput = styled('input')({
 function ProfileEdit() {
 
     const candidateData = useSelector(state => state.data.candidateData);
-    const [count, setCount] = useState(0);
-    const [responseStatus, setResponseStatus] = useState("");
-    const incrementCount = () => {
-        setCount(count + 1);
-    };
 
 
-    const [newProfile, setNewProfile] = useState({
-        id: candidateData.id,
-        first_name: candidateData.firstName,
-        surname: candidateData.surname,
-        current_position: candidateData.currentPosition,
-        email: candidateData.email,
-        phone: candidateData.phone,
-        postcode: candidateData.postcode,
-        town: candidateData.town,
-        city: candidateData.city,
-        gender: candidateData.gender,
-        password: ""
-    });
+
+        const [newProfile, setNewProfile] = useState({
+            id: candidateData.id,
+            first_name: candidateData.firstName,
+            surname: candidateData.surname,
+            current_position: candidateData.currentPosition,
+            email: candidateData.email,
+            phone: candidateData.phone,
+            postcode: candidateData.postcode,
+            town: candidateData.town,
+            city: candidateData.city,
+            gender: candidateData.gender,
+            password: ""
+        });
 
 
-    const handleSubmit = (e) => {
-        console.log(newProfile)
-        e.preventDefault();
-        candidateService.updateUser(newProfile)
-            .then(response => {
-                delete response.data.password //TODO: change backend to remove password from response
-                dispatch(setCandidate(newProfile))
-                dispatch(closeProfileEdit())
-            })
-            .catch(error => {
-                // dispatch(closeProfileEdit())
-                // TODO: call alert function if response.data.status == 401
+        const handleSubmit = (e) => {
+            console.log(newProfile)
+            e.preventDefault();
+            candidateService.updateUser(newProfile)
+                .then(response => {
+                    delete response.data.password //TODO: change backend to remove password from response
+                    dispatch(setCandidate(newProfile))
+                    dispatch(closeProfileEdit())
+                })
+                .catch(error => {
+                    // dispatch(closeProfileEdit())
+                    // TODO: call alert function if response.data.status == 401
 
-                setResponseStatus(error.response.status)
-                incrementCount()
+                   
 
-                console.log(error.response.status)
-            });
-    };
+                    console.log(error.response.status)
+                });
+        };
 
 
 
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setNewProfile({ ...newProfile, [name]: value });
-        console.log(newProfile.name)
+        const handleChange = (e) => {
+            const { name, value } = e.target;
+            setNewProfile({ ...newProfile, [name]: value });
+            console.log(newProfile.name)
 
-    };
+        };
 
-    const dispatch = useDispatch()
+        const dispatch = useDispatch()
 
-    return (
-        <div>
-            <Modal
-                show={true}
-                size="l"
-                aria-labelledby="contained-modal-title-vcenter"
-                centered
-            >
-                <Modal.Header>
+        return (
+            <div>
+                <Modal
+                    show={true}
+                    size="l"
+                    aria-labelledby="contained-modal-title-vcenter"
+                    centered
+                >
+                    <Modal.Header>
 
-                    <Modal.Title id="contained-modal-title-vcenter">
-                        Editing Profile
-                    </Modal.Title>
-                    <IoMdCloseCircleOutline style={{
-                        width: 34,
-                        height: 31
-                    }} onClick={() => dispatch(closeProfileEdit())} />
-                </Modal.Header>
-                <Modal.Body>
-                    <div className={styles.divCenter}>
-                        <Row>
+                        <Modal.Title id="contained-modal-title-vcenter">
+                            Editing Profile
+                        </Modal.Title>
+                        <IoMdCloseCircleOutline style={{
+                            width: 34,
+                            height: 31
+                        }} onClick={() => dispatch(closeProfileEdit())} />
+                    </Modal.Header>
+                    <Modal.Body>
+                        <div className={styles.divCenter}>
+                            <Row>
 
-                            <Col>
-                                <Image src="profile.png" roundedCircle className={styles.profilePhoto} />
-                            </Col>
-
-
-
-                            <div className={styles.UploadNewPhoto}>
-                                <ButtonMUI
-
-                                    component="label"
-                                    role={undefined}
-                                    variant="contained"
-                                    tabIndex={-1}
-                                    startIcon={<CloudUploadIcon />}
-                                >
-                                    Upload Photo
-                                    <VisuallyHiddenInput type="file" name="photo" value={newProfile.photo} onChange={handleChange} />
-
-                                </ButtonMUI>
-                            </div>
+                                <Col>
+                                    <Image src="profile.png" roundedCircle className={styles.profilePhoto} />
+                                </Col>
 
 
 
-                            <div className={styles.divCenter}>
-                                <FaFilePdf className={styles.pdfIcon} />
-                                <ButtonMUI
-                                    component="label"
-                                    role={undefined}
-                                    variant="contained"
-                                    tabIndex={-1}
-                                    startIcon={<CloudUploadIcon />}
-                                    className={styles.buttonmui}
-                                >
-                                    Upload CV
-                                    <VisuallyHiddenInput type="file" name="cv" value={newProfile.cv} onChange={handleChange} />
+                                <div className={styles.UploadNewPhoto}>
+                                    <ButtonMUI
 
-                                </ButtonMUI>
-                            </div>
+                                        component="label"
+                                        role={undefined}
+                                        variant="contained"
+                                        tabIndex={-1}
+                                        startIcon={<CloudUploadIcon />}
+                                    >
+                                        Upload Photo
+                                        <VisuallyHiddenInput type="file" name="photo" value={newProfile.photo} onChange={handleChange} />
 
+                                    </ButtonMUI>
+                                </div>
 
 
-                            <Table>
+
+                                <div className={styles.divCenter}>
+                                    <FaFilePdf className={styles.pdfIcon} />
+                                    <ButtonMUI
+                                        component="label"
+                                        role={undefined}
+                                        variant="contained"
+                                        tabIndex={-1}
+                                        startIcon={<CloudUploadIcon />}
+                                        className={styles.buttonmui}
+                                    >
+                                        Upload CV
+                                        <VisuallyHiddenInput type="file" name="cv" value={newProfile.cv} onChange={handleChange} />
+
+                                    </ButtonMUI>
+                                </div>
+
+
+
+                                <Table>
 
 
 
 
 
-                            </Table>
+                                </Table>
 
 
-                            <FloatingLabel
-                                className={styles.FloatingLabel}
-                                controlId="floatingInput"
-                                label="Name">
-                                <Form.Control type="text" value={newProfile.first_name} name="first_name" onChange={handleChange} />
-                            </FloatingLabel>
-                            <FloatingLabel
-                                className={styles.FloatingLabel}
-                                controlId="floatingInput"
-                                label="Surname">
-                                <Form.Control type="text" value={newProfile.surname} name="surname" onChange={handleChange} />
-                            </FloatingLabel>
-                            <FloatingLabel
-                                className={styles.FloatingLabel}
-                                controlId="floatingInput"
-                                label="Current Position">
-                                <Form.Control type="text" value={newProfile.current_position} name="current_position" onChange={handleChange} />
-                            </FloatingLabel>
-                            <FloatingLabel
-                                className={styles.FloatingLabel}
-                                controlId="floatingInput"
-                                label="Email">
-                                <Form.Control type="email" value={newProfile.email} name="email" onChange={handleChange} />
-                            </FloatingLabel>
-                            <FloatingLabel
-                                className={styles.FloatingLabel}
-                                controlId="floatingInput"
-                                label="Phone">
-                                <Form.Control type="text" value={newProfile.phone} name="phone" onChange={handleChange} />
-                            </FloatingLabel>
-                            <FloatingLabel
-                                className={styles.FloatingLabel}
-                                controlId="floatingInput"
-                                label="Post Code">
-                                <Form.Control type="text" value={newProfile.postcode} name="postcode" onChange={handleChange} />
-                            </FloatingLabel>
-                            <FloatingLabel
-                                className={styles.FloatingLabel}
-                                controlId="floatingInput"
-                                label="Town">
-                                <Form.Control type="text" value={newProfile.town} name="town" onChange={handleChange} />
-                            </FloatingLabel>
-                            <FloatingLabel
-                                className={styles.FloatingLabel}
-                                controlId="floatingInput"
-                                label="City">
-                                <Form.Control type="text" value={newProfile.city} name="city" onChange={handleChange} />
-                            </FloatingLabel>
-                            <FloatingLabel
-                                className={styles.passwordInput}
-                                controlId="floatingInput"
-                                label="Current Password">
-                                <Form.Control type="password" value={newProfile.password} name="password" onChange={handleChange} />
-                            </FloatingLabel>
-                        </Row>
-                    </div>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button onClick={() => dispatch(closeProfileEdit())} >Close</Button>
-                    <Button onClick={handleSubmit}>Update</Button>
+                                <FloatingLabel
+                                    className={styles.FloatingLabel}
+                                    controlId="floatingInput"
+                                    label="Name">
+                                    <Form.Control type="text" value={newProfile.first_name} name="first_name" onChange={handleChange} />
+                                </FloatingLabel>
+                                <FloatingLabel
+                                    className={styles.FloatingLabel}
+                                    controlId="floatingInput"
+                                    label="Surname">
+                                    <Form.Control type="text" value={newProfile.surname} name="surname" onChange={handleChange} />
+                                </FloatingLabel>
+                                <FloatingLabel
+                                    className={styles.FloatingLabel}
+                                    controlId="floatingInput"
+                                    label="Current Position">
+                                    <Form.Control type="text" value={newProfile.current_position} name="current_position" onChange={handleChange} />
+                                </FloatingLabel>
+                                <FloatingLabel
+                                    className={styles.FloatingLabel}
+                                    controlId="floatingInput"
+                                    label="Email">
+                                    <Form.Control type="email" value={newProfile.email} name="email" onChange={handleChange} />
+                                </FloatingLabel>
+                                <FloatingLabel
+                                    className={styles.FloatingLabel}
+                                    controlId="floatingInput"
+                                    label="Phone">
+                                    <Form.Control type="text" value={newProfile.phone} name="phone" onChange={handleChange} />
+                                </FloatingLabel>
+                                <FloatingLabel
+                                    className={styles.FloatingLabel}
+                                    controlId="floatingInput"
+                                    label="Post Code">
+                                    <Form.Control type="text" value={newProfile.postcode} name="postcode" onChange={handleChange} />
+                                </FloatingLabel>
+                                <FloatingLabel
+                                    className={styles.FloatingLabel}
+                                    controlId="floatingInput"
+                                    label="Town">
+                                    <Form.Control type="text" value={newProfile.town} name="town" onChange={handleChange} />
+                                </FloatingLabel>
+                                <FloatingLabel
+                                    className={styles.FloatingLabel}
+                                    controlId="floatingInput"
+                                    label="City">
+                                    <Form.Control type="text" value={newProfile.city} name="city" onChange={handleChange} />
+                                </FloatingLabel>
+                                <FloatingLabel
+                                    className={styles.passwordInput}
+                                    controlId="floatingInput"
+                                    label="Current Password">
+                                    <Form.Control type="password" value={newProfile.password} name="password" onChange={handleChange} />
+                                </FloatingLabel>
+                            </Row>
+                        </div>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button onClick={() => dispatch(closeProfileEdit())} >Close</Button>
+                        <Button onClick={handleSubmit}>Update</Button>
 
-                </Modal.Footer>
-            </Modal>
+                    </Modal.Footer>
+                </Modal>
 
-            {(responseStatus == 401) ? <AppliedJobs /> : ""} # TODO: danger error
-        </div>
-    );
-}
+       
+            </div >
+        );
+    }
 
 export default ProfileEdit;
